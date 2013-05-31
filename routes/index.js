29494d76e5b2f5d4c56aab35644a304ff2788ee9
subpopular <darekrossman@hotmail.com>
2013-05-31 10:40:58 -0400

    Added more files

--- routes/index.js
+++ routes/index.js
@@ -8,36 +8,34 @@ var childProcess = require('child_process'),
   res.render('index');
 };
 
-exports.search = function(req, res){
+exports.search = function (req, res) {
 
-  if (req._parsedUrl.path.indexOf('escaped_fragment') > -1) {
-    var staticResponse = ''
-    ph = childProcess.exec('phantomjs phantom.js', function (error, stdout, stderr) {
-      if (error) {
-        console.log(error.stack);
-        console.log('Error code: '+error.code);
-        console.log('Signal received: '+error.signal);
-      }
+    if (req._parsedUrl.path.indexOf('escaped_fragment') > -1) {
+        ph = childProcess.exec('phantomjs phantom.js', function (error, stdout, stdin) {
+            if (error) {
+                console.log(error.stack);
+                console.log('Error code: ' + error.code);
+                console.log('Signal received: ' + error.signal);
+            }
 
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(stdout)
-      res.end()
-    });
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(stdout);
+            res.end()
+        });
 
-    ph.on('exit', function (code) {
+        ph.on('exit', function (code) {
 
 
-    });
+        });
 
-  } else {
-    console.log('no fragment')
-    res.render('index', {searchKeyword: req.query.q});
-  }
-}
+    } else {
+        res.render('index', {searchKeyword: req.query.q});
+    }
+};
 
-exports.searchCategory = function(req, res){
-  res.render('index', {searchKeyword: req.params.cat});
-}
+exports.searchCategory = function (req, res) {
+    res.render('index', {searchKeyword: req.params.cat});
+};
 
 exports.partials = function (req, res) {
   var name = req.params.name;
